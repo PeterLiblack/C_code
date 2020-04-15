c00a5279942b4b46a8ba41ad586937db2c53c98c
PeterLiblack <812769273@qq.com>
2020-04-15 15:32:54 +0800

    练习

---
+++ Project4/Project4/test.c
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//检查{}的准确性
+//int main()
+//{
+//	int ch;
+//	int braces = 0;
+//
+//	while ((ch=getchar())!=EOF)
+//	{
+//		if (ch = '{')
+//			braces += 1;
+//		if (ch = '}')
+//		if (braces = 0)
+//			printf("Extra closing brace!\n");
+//		else
+//			braces = -1;
+//	}
+//	if (braces > 0)
+//		printf("%d unmatched opening brace(s)!\n", braces);
+//
+//	return EXIT_SUCCESS;
+//}
+
+//输入一个数计算平方根
+//int main()
+//{
+//	float new_guess;
+//	float last_guess;
+//	float number;
+//
+//	printf("Enter a number: ");
+//	scanf("%f", &number);
+//	if (number < 0)
+//	{
+//		printf("Cannot computer the square root of a negative number!\n");
+//		return EXIT_FAILURE;
+//	}
+//
+//	new_guess = 1;
+//	do{
+//		last_guess = new_guess;
+//		new_guess = (last_guess + number / last_guess) / 2;
+//		printf("%.15e\n", new_guess);
+//	} while (new_guess != last_guess);
+//
+//	printf("Square root of %g is %g\n", number, new_guess);
+//
+//	return EXIT_SUCCESS;
+//}
+
+
+int main()
+{
+
+}
